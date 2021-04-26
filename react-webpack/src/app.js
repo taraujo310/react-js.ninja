fc448d2e12a2259b3b346cf7659817761be4335a
Thiago Ururay <tufa.araujo@gmail.com>
2021-04-26 03:40:29 -0300

    MRW#10 - Adiciona o Standard como linter

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,11 +1,11 @@
-'use strict';
+'use strict'
 
-import React from 'react';
+import React from 'react'
 
 const App = React.createClass({
-  render: function() {
-    return <h1>Aplicação</h1>;
+  render: function () {
+    return <h1>Aplicação</h1>
   }
-});
+})
 
-export default App;
+export default App
--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -1,28 +1,28 @@
 'use strict'
 
-import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import App from './app';
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './app'
 
 const renderApp = (NextApp) => {
   return render(
     <AppContainer>
-      <NextApp/>
+      <NextApp />
     </AppContainer>,
     document.querySelector('[data-js="app"]')
   )
 }
 
-renderApp(App);
+renderApp(App)
 
-if(module.hot) {
+if (module.hot) {
   module.hot.accept('./app', () => {
     const NextApp = require('./app').default
 
     render(
       <AppContainer>
-        <NextApp/>
+        <NextApp />
       </AppContainer>,
       document.querySelector('[data-js="app"]')
     )
